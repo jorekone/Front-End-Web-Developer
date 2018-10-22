3652efd014a4a2b37d45b6f56492c81bd31520e1
J <jorgedontstop@gmai.com>
2018-10-22 20:33:07 +0100

    First project

---
+++ Build a portfolio/js/menu-button.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var menu = document.querySelector('#menu');
+  var main = document.querySelector('main');
+  var drawer = document.querySelector('.nav');
+
+  menu.addEventListener('click', function(e) {
+    drawer.classList.toggle('open');
+    e.stopPropagation();
+  });
+  main.addEventListener('click', function() {
+    drawer.classList.remove('open');
+  });
+});
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Front-End-Web-Developer
-This repository is for the Front End Web Developer course
+This repository is for the Front End Web Developer course. Specially for the projects
